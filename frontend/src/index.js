238581f78aae5b3c1a1e3f4d4dc061c7a5731b26
Kushmeet Singh Saluja <32317407+kush-saluja@users.noreply.github.com>
2025-08-04 00:34:54 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter basename="/portfolio">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
